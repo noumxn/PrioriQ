a5871c0680779d5a694f88edc4c8545774139590
nouman <syednouman2114@gmail.com>
2023-03-17 11:06:00 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+import configRoutes from './routes/index.js';
+
+app.use(express.json());
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
