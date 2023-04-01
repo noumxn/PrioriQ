1cd267674e059b1673959b9572ad49d4b4b7005f
markfalletta <markcfalletta@gmail.com>
2023-04-01 02:34:24 -0400

    small edits to data/tasks

--- data/tasks.js
+++ data/tasks.js
@@ -17,7 +17,7 @@ const exportedMethods = {
    * @param {assignedTo} array
    * @description This function creates a new task object and stores it in the database
    * @throws {INTERNAL_SERVER_ERROR} if all valid params are provided but function fails to create a task
-   * @return {task} Returns the task that was just created
+   * @return {boardWithNewTask} Returns the board document with the newly created task in the toDo array
    **/
   async createTask(boardId, taskName, priority, difficulty, estimatedTime, deadline, description, assignedTo) {
     validation.parameterCheck(boardId, taskName, priority, difficulty, estimatedTime, deadline, description, assignedTo);
@@ -42,25 +42,29 @@ const exportedMethods = {
     }
 
     const boardCollection = await boards();
-    await boardCollection.findOneAndUpdate({_id: new ObjectId(boardId)}, {$push: {toDo: newTask}}); //How to check if this call succeeds?
-    const task = await this.getTaskById(newTask._id);
+    const boardWithNewTask = await boardCollection.findOneAndUpdate(
+    {_id: new ObjectId(boardId)}, 
+    {$push: {toDo: newTask}},
+    {returnNewDocument: true});
+    if (!boardWithNewTask) throw validation.throwErr('INTERNAL_SERVER_ERROR', `Could not insert new task into board.`)
 
-    return task;
+    return boardWithNewTask;
   },
   /*
    * @param {taskId} string
    * @description This function finds a task document by its id
-   * @throws {INTERNAL_SERVER_ERROR} if all valid params are provided but function fails to find a task
+   * @throws {NOT_FOUND} if all valid params are provided but function fails to find a task
    * @return {task} Returns the task that was just found
    **/
   async getTaskById(taskId) {
+    validation.parameterCheck(taskId);
     validation.idCheck(taskId);
 
     const boardCollection = await boards();
     const foundTask = await boardCollection.findOne(
     {$or: [{'toDo._id': new ObjectId(taskId)}, {'inProgress._id': new ObjectId(taskId)}, {'done._id': new ObjectId(taskId)}]},
     {_id: 0, 'toDo.$': 1, 'inProgress.$': 1, 'done._id': 1});
-    if (!foundTask) throw validation.throwErr('INTERNAL_SERVER_ERROR', `No task with given id found.`); //What kind of error/if at all should this be?
+    if (!foundTask) throw validation.throwErr('NOT_FOUND', `No task with given id found.`); //What kind of error/if at all should this be?
     
     if (foundTask.toDo.length === 0 && foundTask.inProgress === 0) {
       foundTask.done[0]._id = foundTask.done[0]._id.toString();
@@ -84,7 +88,7 @@ const exportedMethods = {
    * @param {description} string 
    * @param {assignedTo} array
    * @description This function updates the task object defined by taskId within the database
-   * @throws {INTERNAL_SERVER_ERROR} if all valid params are provided but function fails to update a task
+   * @throws {NOT_FOUND} if all valid params are provided but function fails to update a task
    * @return {task} Returns the updated task.
    **/
   async updateTask(taskId, taskName, priority, difficulty, estimatedTime, deadline, description, assignedTo) {
@@ -110,14 +114,14 @@ const exportedMethods = {
       assignedTo: assignedTo
   }
 
-  const boardCollection = await boards();
+    const boardCollection = await boards();
     const updatedInfo = await boardCollection.findOneAndUpdate(
     {$or: [{'toDo._id': new ObjectId(taskId)}, {'inProgress._id': new ObjectId(taskId)}, {'done._id': new ObjectId(taskId)}]},
     {$set: [{_id: taskId}, {taskName: taskName}, {priority: priority}, {difficulty: difficulty}, {estimatedTime: estimatedTime},
     {deadline: deadline}, {description: description}, {assignedTo: assignedTo}]},
     {returnDocument: 'after'});
     if (updatedInfo.lastErrorObject.n === 0) {
-      throw validation.throwErr('INTERNAL_SERVER_ERROR', `No task with given id found.`);
+      throw validation.throwErr('NOT_FOUND', `No task with given id found.`);
     };
   
     return await this.getTaskById(updatedTask._id);
@@ -125,21 +129,22 @@ const exportedMethods = {
    /*
    * @param {taskId} string
    * @description This function deletes the task with the given id from the database
-   * @throws {INTERNAL_SERVER_ERROR} if all valid params are provided but function fails to delete a task
+   * @throws {NOT_FOUND} if all valid params are provided but function fails to delete a task
    * @return {task} Returns the updated board with the successfully deleted task
    **/
   async deleteTask(taskId) {
+    validation.parameterCheck(taskId);
     validation.idCheck(taskId);
 
-    const foundTask = await boardCollection.updateOne(
+    const updatedBoard = await boardCollection.updateOne(
     {$or: [{'toDo._id': new ObjectId(taskId)}, {'inProgress._id': new ObjectId(taskId)}, {'done._id': new ObjectId(taskId)}]},
     {$pull: {toDo: {_id: new ObjectId(taskId)} }}, 
     {$pull: {inProgress: {_id: new ObjectId(taskId)}} },
     {$pull: {done: {_id: new ObjectId(taskId)}} },
     {returnNewDocument: true});
-    if (!foundTask) throw validation.throwErr('INTERNAL_SERVER_ERROR', `No task with given id found.`); //What kind of error/if at all should this be?
+    if (!updatedBoard) throw validation.throwErr('NOT_FOUND', `No task with given id found in board.`);
 
-    return foundTask;
+    return updatedBoard;
   },
 };
 
