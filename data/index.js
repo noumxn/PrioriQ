d2fb7b925139dcfc91b72be06c97cf40a29b40d3
nouman <syednouman2114@gmail.com>
2023-04-06 23:05:57 -0400

    update app.js

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 import userDataFunctions from './users.js';
 import boardDataFunctions from './boards.js';
 import taskDataFunctions from './tasks.js';
+import helpers from './helpers.js';
 
 export const userData = userDataFunctions;
 export const boardData = boardDataFunctions;
 export const taskData = taskDataFunctions;
+export const helpers = helpers;
