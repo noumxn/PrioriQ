e0485df4f8b3c22f88200689fe60a297b8059b25
nouman <syednouman2114@gmail.com>
2023-04-22 01:19:44 -0400

    completeCheckListItem() and addTaskToCheckList()

--- data/checkList.js
+++ data/checkList.js
@@ -3,14 +3,50 @@ import {users} from '../config/mongoCollections.js';
 import {boards} from '../config/mongoCollections.js';
 import userData from './users.js'
 import boardData from './boards.js';
-import taskData from './boards.js';
+import taskData from './tasks.js';
 import validation from '../utils/validation.js';
 import helpers from './helpers.js';
 
+
 const exportedMethods = {
-  async createCheckListItem(taskId, taskName, priority, difficulty, completed) {},
-  async addTaskToCheckList() {},
-  async deleteTaskFromCheckList() {},
+  async addTaskToCheckList(taskId, username) {
+    const task = await taskData.getTaskById(taskId);
+    const checkListItem = {
+      taskId: task._id,
+      taskName: task.taskName,
+      completed: false
+    }
+    const user = await userData.getUserByUsername(username);
+    user.checkList.push(checkListItem);
+
+    const userCollection = await users();
+    const updatedUser = await userCollection.updateOne(
+      {username: username},
+      {$set: {checkList: user.checkList}}
+    );
+    console.log(updatedUser);
+
+    return await userData.getUserByUsername(username);
+  },
+
+  async completeCheckListItem(taskId, username) {
+    const user = await userData.getUserByUsername(username);
+    user.checkList.forEach(checkListItem => {
+      if (checkListItem.taskId === taskId) {
+        checkListItem.completed = true;
+      }
+    })
+
+    const userCollection = await users();
+    await userCollection.updateOne(
+      {username: username},
+      {$set: {checkList: user.checkList}}
+    )
+
+    return await userData.getUserByUsername(username);
+  },
+  async deleteTasksFromCheckList() {},
+  async updateCheckListItem() {}
 };
 
 export default exportedMethods;
--- tasks/seed.js
+++ tasks/seed.js
@@ -1,7 +1,9 @@
+import {checkDataType} from 'ajv/dist/compile/validate/dataType.js';
 import {closeConnection, dbConnection} from '../config/mongoConnection.js';
 import {userData} from '../data/index.js';
 import {boardData} from '../data/index.js';
 import {taskData} from '../data/index.js';
+import {checkListData} from '../data/index.js';
 
 (async () => {
   let tom = undefined;
@@ -36,21 +38,33 @@ import {taskData} from '../data/index.js';
   console.log("└─ Adding Tasks...");
   try {
     task1 = await taskData.createTask(tomBoard._id, "task1", 1, null, "00 hour 01 mins", "2023-04-19T23:26:24.864Z", "This is a test task1", ["tom_smith"]);
-    task2 = await taskData.createTask(tomBoard._id, "task2", 2, null, "00 hour 10 mins", "2023-04-19T23:51:24.864Z", "This is a test task2", ["tom_smith"]);
-    task3 = await taskData.createTask(tomBoard._id, "task3", 3, null, "00 hour 01 mins", "2023-04-19T23:27:24.864Z", "This is a test task3", ["tom_smith"]);
-    task4 = await taskData.createTask(tomBoard._id, "task4", 8, null, "00 hour 01 mins", "2023-04-19T23:31:24.864Z", "This is a test task4", ["tom_smith"]);
-    task5 = await taskData.createTask(tomBoard._id, "task5", 10, null, "00 hour 02 mins", "2023-04-19T23:31:24.864Z", "This is a test task5", ["tom_smith"]);
+    // task2 = await taskData.createTask(tomBoard._id, "task2", 2, null, "00 hour 10 mins", "2023-04-19T23:51:24.864Z", "This is a test task2", ["tom_smith"]);
+    // task3 = await taskData.createTask(tomBoard._id, "task3", 3, null, "00 hour 01 mins", "2023-04-19T23:27:24.864Z", "This is a test task3", ["tom_smith"]);
+    // task4 = await taskData.createTask(tomBoard._id, "task4", 8, null, "00 hour 01 mins", "2023-04-19T23:31:24.864Z", "This is a test task4", ["tom_smith"]);
+    // task5 = await taskData.createTask(tomBoard._id, "task5", 10, null, "00 hour 02 mins", "2023-04-19T23:31:24.864Z", "This is a test task5", ["tom_smith"]);
     let boardWith5Tasks = await boardData.getBoardById(tomBoard._id)
     console.log(boardWith5Tasks);
   } catch (e) {
     console.log(e);
   }
+  // try {
+  //   await taskData.moveToInProgress(task2._id);
+  //   await taskData.moveToInProgress(task4._id);
+  //   await taskData.moveToDone(task5._id);
+  // } catch (e) {
+  //   console.log(e);
+  // }
   try {
-    await taskData.moveToInProgress(task2._id);
-    await taskData.moveToInProgress(task4._id);
-    await taskData.moveToDone(task5._id);
-    let updatedBoard = await boardData.getBoardById(tomBoard._id);
-    console.log(updatedBoard);
+    await checkListData.addTaskToCheckList(task1._id, "tom_smith");
+    let updatedUser = await userData.getUserByUsername('tom_smith');
+    console.log(updatedUser);
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    await checkListData.completeCheckListItem(task1._id, "tom_smith");
+    let updatedUser = await userData.getUserByUsername('tom_smith');
+    console.log(updatedUser);
   } catch (e) {
     console.log(e);
   }
