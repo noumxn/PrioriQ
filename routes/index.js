12e0fcdc54beab8ad3365c0c8385ef8c0cd458df
nouman <syednouman2114@gmail.com>
2023-04-24 00:44:09 -0400

    add routes/homepage.js and add imports to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
+import authRoutes from './auth.js';
+import hompageRoutes from './hompage.js';
+import usersettingsRoutes from './userSettings.js';
+import boardRoutes from './boards.js';
+import boardsettingsRoutes from './boardsSettings.js';
 
 const constructorMethod = (app) => {
   app.use('/', authRoutes);
-  app.use('/hompage', hompage);
-  app.use('/usersettings', usersettings);
-  app.use('/boards', boards);
-  app.use('/boardsettings', boardsettings);
+  app.use('/hompage', hompageRoutes);
+  app.use('/usersettings', usersettingsRoutes);
+  app.use('/board', boardRoutes);
+  app.use('/boardsettings', boardsettingsRoutes);
   app.use('*', (req, res) => {
-    res.redirect('/login', loginRoutes).status(404);
+    res.redirect('/', authRoutes);
   });
 };
 
